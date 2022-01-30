3c308302d3e24edcf6885162682b4dcdbd79f941
AnastasiiaPosulikhina <anastasiia.posulikhina@jetbrains.com>
2022-01-30 20:05:25 +0300

    First commit

---
+++ Parser/Parser.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Text;
+
+namespace Parser;
+
+public class InfoParser : IParse
+{
+    public string Parse(string URL)
+    {
+        StringBuilder result = new StringBuilder();
+        
+        try
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = "GET";
+            request.Accept = "application/json";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            result.Append(reader.ReadToEnd());
+            response.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("The exception was thrown: " + ex.Message);
+        }
+
+        return result.ToString();
+    }
+}
---
+++ Parser/Program.cs
@@ -0,0 +1,7 @@
+﻿using Parser;
+
+InfoParser parser = new InfoParser();
+Converter converter = new Converter();
+
+string result = parser.Parse("https://search-maps.yandex.ru/v1/?apikey=a31ab3e4-acea-4b6f-be2e-c004f538e78b&text=Санкт-Петербург, Адмиралтейский район, общеобразовательная школа №&lang=ru_RU&results=500");
+converter.ConvertToJson("C:\\Users\\Anastasiia.Posulikhi\\RiderProjects\\IndexiumUI_\\Parser\\AdmiralteiskyDistrictSchools.json", result);
